2e9c62fe630e94e31761649a10b7278476c71ad6
Mateusz Łopaciński <lop.mateusz.2001@gmail.com>
2025-08-26 22:03:22 +0200

    Some typetests still failing

--- packages/react-native-reanimated/src/commonTypes.ts
+++ packages/react-native-reanimated/src/commonTypes.ts
@@ -454,8 +454,8 @@ export type StyleUpdaterContainer = RefObject<
 
 type GetProp<T, K extends PropertyKey> = K extends keyof T ? T[K] : undefined;
 
-type ScrollResponderType = Partial<
-  InternalHostInstance &
+type ScrollResponderType = InternalHostInstance &
+  Partial<
     ReturnType<
       NonNullable<
         | GetProp<ScrollView, 'getScrollResponder'>
@@ -463,8 +463,8 @@ type ScrollResponderType = Partial<
         | GetProp<SectionList, 'getScrollResponder'>
       >
     > &
-    JSX.Element
->;
+      JSX.Element
+  >;
 
 export type InternalHostInstance = Partial<
   HostInstance & {
